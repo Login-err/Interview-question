048861651a4d9cf1bbb760f65e680d46c7ce2418
Login-err <846261654@qq.com>
2019-10-24 09:00:44 +0800

    答案稍作修改

--- day_9/app.js
+++ day_9/app.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const wss = require('websocket').server;
-const app = express();
-app.get('socket', (req, res) => {
-    res.send({ a: '1' });
-});
-app.listen('3000', (err) => {
-    if (err) {
-        return;
-    }
-    console.log('服务器连接成功');
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8075 });
+wss.on('connection', (ws) => {
+    ws.on('message', (msg) => {
+        console.log(msg);
+    });
 });
